5e41bbb42b4f9dc5b4d0fcca1ace7b6e38794458
Karen Marlen <72042911+karen-marlen@users.noreply.github.com>
2021-02-02 11:23:53 -0300

    creating pages

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import { Navbar } from '../components/Navbar'
+import styles from '../styles/Home.module.css'
+
+export default function About() {
+  return (
+
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <h1>About</h1>
+    </div>
+  );
+}
