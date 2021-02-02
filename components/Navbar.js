6e51472ba9a50fa1ce3ff2d2788e0701f199a870
Karen Marlen <72042911+karen-marlen@users.noreply.github.com>
2021-02-02 11:36:21 -0300

    adding styled components

---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+    height: 80px;
+    background: #000;
+`;
+
+export const Navbar = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    )
+}
